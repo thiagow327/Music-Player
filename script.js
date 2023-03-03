efd06bfbfeb60975defecf58bfcab66500160e29
Thiago Silva <84516746+thiagow327@users.noreply.github.com>
2023-03-03 15:14:47 -0300

    feat (playPauseDecider): função de play e pause

--- script.js
+++ script.js
@@ -3,11 +3,33 @@ const play = document.getElementById('play');
 const songName = document.getElementById('song-name');
 const bandName = document.getElementById('band-name');
 
+let isPlaying = false;
+
 songName.innerText = "Lofi and Coffe";
 bandName.innerText = "Jon J.Morin"
 
+
 function playSong() {
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     song.play();
+    isPlaying = true;
 }
 
-play.addEventListener('click', playSong);
+function pauseSong() {
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying == true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+play.addEventListener('click', playPauseDecider);
