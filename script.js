818b98ef1ba2f47aab1b66d0fdf03b3750f068e6
Thiago Silva <84516746+thiagow327@users.noreply.github.com>
2023-03-09 15:41:51 -0300

    feat(likeButtonRender e likeButtonClicked) funcionalidade do botão de like para favoritar as músicas

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const cover = document.getElementById("cover");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
 const previous = document.getElementById("previous");
+const likeButton = document.getElementById("like");
 const currentProgress = document.getElementById("current-progress");
 const progressContainer = document.getElementById("progress-container");
 const shuffleButton = document.getElementById("shuffle");
@@ -16,16 +17,19 @@ const lofi = {
   songName: "Lofi and Coffee",
   file: "Lofi and Coffee",
   artist: "Jon J. Morin",
+  liked: false,
 };
 const jazz = {
   songName: "F. Juice",
   file: "F. Juice",
   artist: "Jazzfixie",
+  liked: true,
 };
 const rock = {
   songName: "Rock N Roll",
   file: "Rock N Roll",
   artist: "Shadow",
+  liked: false,
 };
 
 let isPlaying = false;
@@ -57,11 +61,25 @@ function playPauseDecider() {
   }
 }
 
+function likeButtonRender() {
+  if(sortedPlaylist[index].liked === true) {
+    likeButton.querySelector('.bi').classList.remove('bi-heart');
+    likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+    likeButton.classList.add('button-active');
+  }
+  else {
+    likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+    likeButton.querySelector('.bi').classList.add('bi-heart');
+    likeButton.classList.remove('button-active');
+  }
+}
+
 function initializeSong() {
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
   cover.src = `/images/${sortedPlaylist[index].file}.png`;
   song.src = `/songs/${sortedPlaylist[index].file}.mp3`;
+  likeButtonRender();
 }
 
 function previousSong() {
@@ -152,6 +170,15 @@ function updateTotalTime() {
   totalTime.innerText = toHHMMSS(song.duration);
 }
 
+function likeButtonClicked() {
+  if(sortedPlaylist[index].liked === false) {
+    sortedPlaylist[index].liked = true;
+  } else {
+    sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+}
+
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
@@ -163,3 +190,4 @@ song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButtonClicked);
 repeatButton.addEventListener("click", repeatButtonClicked);
+likeButton.addEventListener("click", likeButtonClicked);
