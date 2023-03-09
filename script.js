a7c2e2d88875e68e47f4f98ed563dd1bc4b8c66b
Thiago Silva <84516746+thiagow327@users.noreply.github.com>
2023-03-09 12:56:51 -0300

    feat(repeatButtonClicked e nextOrRepeat): funcionalidade do botão repeat para repetir a música

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const previous = document.getElementById("previous");
 const currentProgress = document.getElementById("current-progress");
 const progressContainer = document.getElementById("progress-container");
 const shuffleButton = document.getElementById("shuffle");
+const repeatButton = document.getElementById("repeat");
 
 const lofi = {
   songName: "Lofi and Coffee",
@@ -27,6 +28,7 @@ const rock = {
 
 let isPlaying = false;
 let isShuffled = false;
+let repeatOn = false;
 const originalPlaylist = [lofi, jazz, rock];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
@@ -117,11 +119,31 @@ function shuffleButtonClicked() {
   }
 }
 
+function repeatButtonClicked() {
+  if(repeatOn === false) {
+    repeatOn = true;
+    repeatButton.classList.add("button-active");
+  } else {
+    repeatOn = false;
+    repeatButton.classList.remove("button-active");
+  }
+}
+
+function nextOrRepeat() {
+  if(repeatOn === false) {
+    nextSong();
+  } else {
+    playSong();
+  }
+}
+
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
 previous.addEventListener("click", previousSong);
 next.addEventListener("click", nextSong);
 song.addEventListener("timeupdate", updateProgressBar);
+song.addEventListener("ended", nextOrRepeat);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButtonClicked);
+repeatButton.addEventListener("click", repeatButtonClicked);
